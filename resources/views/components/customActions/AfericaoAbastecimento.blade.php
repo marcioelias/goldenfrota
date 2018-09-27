6702b9acd6f7c43d82902b146ca227bcdadad63f
marcioelias <marcioelias@gmail.com>
2018-09-27 01:43:46 -0300

    Alteração do Icone #45

--- resources/views/components/customActions/AfericaoAbastecimento.blade.php
+++ resources/views/components/customActions/AfericaoAbastecimento.blade.php
@@ -1,3 +1,3 @@
 <a class="btn btn-xs btn-success" href="{{ route('afericao.create', ['abastecimento' => $data->id]) }}" target="_blank">
-    <i class="glyphicon glyphicon-th" data-toggle="tooltip" data-placement="top" title="{{__('Aferição')}}" data-original-title="{{__('Alterar Senha')}}"></i>
+    <i class="glyphicon glyphicon-wrench" data-toggle="tooltip" data-placement="top" title="{{__('Aferição')}}" data-original-title="{{__('Alterar Senha')}}"></i>
 </a>
