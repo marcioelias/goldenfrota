859a526c5c0c31cf5e1f28f0f1b56ecebcaf1e72
marcioelias <marcioelias@gmail.com>
2018-09-27 01:51:47 -0300

    Mais um ajuste no Icone #45

--- resources/views/components/customActions/AfericaoAbastecimento.blade.php
+++ resources/views/components/customActions/AfericaoAbastecimento.blade.php
@@ -1,3 +1,9 @@
-<a class="btn btn-xs btn-success" href="{{ route('afericao.create', ['abastecimento' => $data->id]) }}" target="_blank">
-    <i class="glyphicon glyphicon-wrench" data-toggle="tooltip" data-placement="top" title="{{__('Aferição')}}" data-original-title="{{__('Alterar Senha')}}"></i>
-</a>
+@if ($data->eh_afericao)
+    <button class="btn btn-xs btn-success" disabled>
+        <i class="glyphicon glyphicon-wrench" data-toggle="tooltip" data-placement="top" title="{{__('Aferição')}}" data-original-title="{{__('Aferição')}}"></i>
+    </button>
+@else
+    <a class="btn btn-xs btn-success" href="{{ route('afericao.create', ['abastecimento' => $data->id]) }}">
+        <i class="glyphicon glyphicon-wrench" data-toggle="tooltip" data-placement="top" title="{{__('Aferição')}}" data-original-title="{{__('Aferição')}}"></i>
+    </a>
+@endif
