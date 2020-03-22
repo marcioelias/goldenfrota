6bb466bf7eabee582d414364146d3b399ab2a0bb
Marcio <marcioelias@gmail.com>
2020-03-21 21:20:37 -0300

    Ajustes layout create/edit atendente

--- resources/views/atendente/create.blade.php
+++ resources/views/atendente/create.blade.php
@@ -21,32 +21,39 @@
                             'required' => true,
                             'autofocus' => true,
                             'inputValue' => isset($atendente->nome_atendente) ? $atendente->nome_atendente : '',
-                            'inputSize' => 6
+                            'inputSize' => 12
                         ]
                     ]
                 ])
                 @endcomponent
-                @component('components.form-group', [
-                    'inputs' => [
-                        [
-                            'type' => 'text',
-                            'field' => 'usuario_atendente',
-                            'label' => 'Usuário',
-                            'required' => true,
-                            'inputValue' => isset($atendente->usuario_atendente) ? $atendente->usuario_atendente : '',
-                            'inputSize' => 6
-                        ],
-                        [
-                            'type' => 'text',
-                            'field' => 'senha_atendente',
-                            'label' => 'Senha',
-                            'required' => true,
-                            'inputValue' => isset($atendente->senha_atendente) ? $atendente->senha_atendente : '',
-                            'inputSize' => 6
-                        ]
-                    ]
-                ])
-                @endcomponent
+                <div class="card">
+                    <div class="card-header">
+                        <strong>Automação (Hiro)</strong>
+                    </div>
+                    <div class="card-body">
+                        @component('components.form-group', [
+                            'inputs' => [
+                                [
+                                    'type' => 'text',
+                                    'field' => 'usuario_atendente',
+                                    'label' => 'Nome',
+                                    'required' => true,
+                                    'inputValue' => isset($atendente->usuario_atendente) ? $atendente->usuario_atendente : '',
+                                    'inputSize' => 6
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'field' => 'senha_atendente',
+                                    'label' => 'Identificação',
+                                    'required' => true,
+                                    'inputValue' => isset($atendente->senha_atendente) ? $atendente->senha_atendente : '',
+                                    'inputSize' => 6
+                                ]
+                            ]
+                        ])
+                        @endcomponent
+                    </div>
+                </div>
             @endsection
         @endcomponent
     </div>
--- resources/views/atendente/edit.blade.php
+++ resources/views/atendente/edit.blade.php
@@ -21,7 +21,7 @@
                             'required' => true,
                             'autofocus' => true,
                             'inputValue' => $atendente->nome_atendente,
-                            'inputSize' => 6
+                            'inputSize' => 11
                         ],
                         [
                             'type' => 'select',
@@ -30,31 +30,39 @@
                             'inputSize' => 1,
                             'indexSelected' => $atendente->ativo,
                             'items' => Array('Não', 'Sim'),
+                            'searchById' => false
                         ]
                     ]
                 ])
                 @endcomponent
-                @component('components.form-group', [
-                    'inputs' => [
-                        [
-                            'type' => 'text',
-                            'field' => 'usuario_atendente',
-                            'label' => 'Usuário',
-                            'required' => true,
-                            'inputValue' => $atendente->usuario_atendente,
-                            'inputSize' => 6
-                        ],
-                        [
-                            'type' => 'text',
-                            'field' => 'senha_atendente',
-                            'label' => 'Senha',
-                            'required' => true,
-                            'inputValue' => $atendente->senha_atendente,
-                            'inputSize' => 6
-                        ]
-                    ]
-                ])
-                @endcomponent
+                <div class="card">
+                    <div class="card-header">
+                        <strong>Automação (Hiro)</strong>
+                    </div>
+                    <div class="card-body">
+                        @component('components.form-group', [
+                            'inputs' => [
+                                [
+                                    'type' => 'text',
+                                    'field' => 'usuario_atendente',
+                                    'label' => 'Usuário',
+                                    'required' => true,
+                                    'inputValue' => $atendente->usuario_atendente,
+                                    'inputSize' => 6
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'field' => 'senha_atendente',
+                                    'label' => 'Senha',
+                                    'required' => true,
+                                    'inputValue' => $atendente->senha_atendente,
+                                    'inputSize' => 6
+                                ]
+                            ]
+                        ])
+                        @endcomponent
+                    </div>
+                </div>
             @endsection
         @endcomponent
     </div>
